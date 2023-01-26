c9ede482bf51bb5f7a8f310273818b3a3b071353
valentinferreyraML <valentin.ferreyra@mercadolibre.com>
2023-01-25 21:34:40 -0300

    fix: implement errors in sql store

--- pkg/store/sql_store.go
+++ pkg/store/sql_store.go
@@ -2,8 +2,22 @@ package store
 
 import (
 	"database/sql"
+	"errors"
 
 	"github.com/bootcamp-go/consignas-go-db.git/internal/domain"
+)
+
+var (
+	ErrNotFound = errors.New("There is not product with that id")
+	ErrInternal = errors.New("Something internal has wrong")
+)
+
+const (
+	GET_BY_ID = `SELECT id, name, quantity, code_value, is_published, expiration, price 
+ 			     FROM products 
+			     WHERE id = ?`
+	INSERT = `INSERT INTO products(id, name, quantity, code_value, is_published, expiration, price)
+			  VALUES ?, ?, ?, ?, ?, ?, ?`
 )
 
 type sqlStore struct {
@@ -14,37 +28,43 @@ func NewSQLStore(db *sql.DB) *sqlStore {
 	return &sqlStore{db}
 }
 
-func (store *sqlStore) Read(id int) (product domain.Product, err error) {
-	query := `SELECT 
-				id, name, quantity, code_value, is_published, expiration, price 
-			 FROM 
-			 	products 
-			WHERE 
-				id = ?`
+func (store *sqlStore) Read(id int) (product domain.Product, er error) {
+	row := store.Database.QueryRow(GET_BY_ID, id)
 
-	rows, err := store.Database.Query(query, id)
-	if err != nil {
-		return domain.Product{}, err
+	if row.Err() != nil {
+		switch row.Err() {
+		case sql.ErrNoRows:
+			er = ErrNotFound
+		case sql.ErrConnDone:
+			er = ErrInternal
+		}
+		return
 	}
 
-	for rows.Next() {
-		if err := rows.Scan(
-			&product.Id,
-			&product.Name,
-			&product.Quantity,
-			&product.CodeValue,
-			&product.IsPublished,
-			&product.Expiration,
-			&product.Price,
-		); err != nil {
-			return domain.Product{}, err
-		}
+	if err := row.Scan(
+		&product.Id,
+		&product.Name,
+		&product.Quantity,
+		&product.CodeValue,
+		&product.IsPublished,
+		&product.Expiration,
+		&product.Price,
+	); err != nil {
+		er = ErrInternal
+		return
 	}
 	return
 }
 
 func (store *sqlStore) Create(product domain.Product) error {
-	return nil
+	statement, err := store.Database.Prepare(INSERT)
+	if err != nil {
+		return err
+	}
+
+	defer statement.Close()
+
+	result, err := statement.Exec()
 }
 
 func (store *sqlStore) Update(product domain.Product) error { return nil }
